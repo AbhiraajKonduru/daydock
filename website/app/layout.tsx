8e7d6ed28dfc10d8661c0aa106a8b3ee5e7a780d
AbhiraajKonduru <raaj.r.services@gmail.com>
2026-08-28 13:12:20 -0400

    fix website rendering on IOS

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -1,4 +1,12 @@
 import type { Metadata } from "next";
+import "@fontsource/dm-mono/400.css";
+import "@fontsource/dm-mono/500.css";
+import "@fontsource/dm-sans/400.css";
+import "@fontsource/dm-sans/500.css";
+import "@fontsource/dm-sans/600.css";
+import "@fontsource/dm-sans/700.css";
+import "@fontsource/merriweather/400.css";
+import "@fontsource/merriweather/400-italic.css";
 import "./globals.css";
 
 export const metadata: Metadata = {
